7d0c543e61b41a99c1be2ef33f118f76e4bcb332
RebecaRibeiro99 <ribeirobeca1999@gmail.com>
2022-05-27 18:22:18 -0300

    adicionando cep

--- fornecedor/src/main/java/com/residencia/comercio/entities/Fornecedor.java
+++ fornecedor/src/main/java/com/residencia/comercio/entities/Fornecedor.java
@@ -75,7 +75,6 @@ public class Fornecedor {
 	private Date dataAbertura;
 
 	@OneToMany(mappedBy = "fornecedor")
-	//@JsonManagedReference
 	private List<Produto> produtoList;
 	
 	public Integer getIdFornecedor() {
--- fornecedor/src/main/java/com/residencia/comercio/entities/Produto.java
+++ fornecedor/src/main/java/com/residencia/comercio/entities/Produto.java
@@ -19,63 +19,60 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 	    property = "idProduto")
 public class Produto {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="id_produto")
-	private Integer idProduto;
-	
-	@Column(name="sku")
-	private String sku;
-	
-	@Column(name="nome_produto")
-	private String nomeProduto;
-	
-	@ManyToOne
-	@JoinColumn(name = "id_fornecedor", referencedColumnName = "id_fornecedor")
-	private Fornecedor fornecedor;
-	
-	@ManyToOne
-	@JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria")
-	private Categoria categoria;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto")
+    private Integer idProduto;
+    @Column(name = "sku")
+    private String sku;
+    @Column(name = "nome_produto")
+    private String nomeProduto;
+    @ManyToOne
+    @JoinColumn(name = "id_fornecedor", referencedColumnName = "id_fornecedor")
+    private Fornecedor fornecedor;
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria")
+    private Categoria categoria;
 
-	public Integer getIdProduto() {
-		return idProduto;
-	}
+    public Integer getIdProduto() {
+        return idProduto;
+    }
 
-	public void setIdProduto(Integer idProduto) {
-		this.idProduto = idProduto;
-	}
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
 
-	public String getSku() {
-		return sku;
-	}
+    public String getSku() {
+        return sku;
+    }
 
-	public void setSku(String sku) {
-		this.sku = sku;
-	}
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
 
-	public String getNomeProduto() {
-		return nomeProduto;
-	}
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
 
-	public void setNomeProduto(String nomeProduto) {
-		this.nomeProduto = nomeProduto;
-	}
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
 
-	public Fornecedor getFornecedor() {
-		return fornecedor;
-	}
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
 
-	public void setFornecedor(Fornecedor fornecedor) {
-		this.fornecedor = fornecedor;
-	}
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
 
-	public Categoria getCategoria() {
-		return categoria;
-	}
+    public Categoria getCategoria() {
+        return categoria;
+    }
 
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
-	}
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
-	
 }
+
+
